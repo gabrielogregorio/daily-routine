a2740b6870dc0191cf4f7472a75f0c87c5f031da
gabriel <gabriel.gregorio.1@outlook.com>
2022-01-20 07:22:05 -0300

    add mvp of task list

---
+++ src/core/services/localstorage.ts
@@ -0,0 +1,19 @@
+function convertToLocastorageData(data: any){
+  return JSON.stringify(data)
+}
+
+export function setLocalstorage(data: any) {
+  localStorage.setItem('data', convertToLocastorageData(data))
+}
+
+export function isEqualData(data: any) {
+  return convertToLocastorageData(data) === localStorage.getItem('data')
+
+}
+
+export function getLocalstorage() {
+  const daTax = localStorage.getItem('data')
+   const data: any =  daTax ? daTax : '[]'
+   return JSON.parse(data)
+}
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,105 @@
-import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css'
 
+import { BiUpArrow, BiDownArrow } from 'react-icons/bi'
+import { getLocalstorage, isEqualData, setLocalstorage } from '../../core/services/localstorage';
+
+type taskListType = {
+  name: string,
+  order: number
+}
+
+const localStorageTaskList:taskListType[] = getLocalstorage()
+
 export const Home = () => {
+  const [newInput, setNewInput] = useState('')
+  const [taskLists, setTaskLists] = useState<taskListType[]>(localStorageTaskList)
+  const taskListsSorted: taskListType[] = taskLists.sort((a, b) => a.order > b.order ? 1 : -1 )
+  const [disableButton, setDisableButton ]= useState(isEqualData(taskLists))
+
+  function handleInput(event: any) {
+    setNewInput(event.target.value)
+  }
+
+
+
+  function handleReturn(event: any) {
+    if(event.key === 'Enter') {
+      let maxOrder = 0
+      if(taskLists.length > 0 ) {
+        const maxOrderObject = taskLists.reduce((max, obj) => (max.order > obj.order) ? max : obj)
+        maxOrder = maxOrderObject.order + 1
+      }
+
+      setTaskLists((prev) => [...prev, {name: newInput, order: maxOrder}])
+      setNewInput('')
+    }
+
+  }
+
+  function deleteTask(taskOrder: number) {
+    const taskListsRemains: taskListType[] = taskLists.filter(task => task.order !== taskOrder)
+    setTaskLists(taskListsRemains)
+  }
+
+  function putPosition(position: 'TOP' | 'BOTTOM', referenceIndex: number) {
+    let data = [...taskLists]
+
+    const base = taskLists[referenceIndex]
+    const bottom = taskLists[referenceIndex + 1]
+    const top = taskLists[referenceIndex - 1]
+    const orderBase = base.order
+
+    if(position === 'BOTTOM' && bottom) {
+      const orderBottom = bottom.order
+
+      data[referenceIndex].order = orderBottom
+      data[referenceIndex + 1].order = orderBase
+    } else  if(position === 'TOP' && top) {
+      const orderTop = top.order
+
+      data[referenceIndex].order = orderTop
+      data[referenceIndex - 1].order = orderBase
+    }
+    setTaskLists(data)
+  }
+
+  function handleLocalstorage() {
+    setLocalstorage(taskLists)
+    setDisableButton(true)
+  }
+
+  useEffect(() => {
+    setDisableButton(isEqualData(taskLists))
+  }, [taskLists])
+
   return (
   <>
-    <Header />
+    {/* <Header /> */}
     <main className={styles.main}>
-      <section aria-label="Conteúdo Principal">
-        <h1>title content</h1>
-        <article>
-          <h2>title of article</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima nam quasi odio facilis debitis suscipit, odit expedita. Impedit eum obcaecati neque illum omnis eius nesciunt autem praesentium, voluptas nemo accusamus?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda qui fugiat officia eaque ipsa corrupti. Tempore praesentium consectetur deleniti quibusdam architecto dolorum ea. Et perspiciatis reiciendis pariatur quos voluptatem ipsam.</p>
-        </article>
-      </section>
+    {taskListsSorted.map(({order, name}, index) => {
+      return <div key={order} className={styles.task}>
+        <button className={styles.task__name}><span>{name}</span></button>
+        <div className={styles.task__arrow}>
+          <button onClick={() => putPosition('TOP', index)}><BiUpArrow /></button>
+          <button onClick={() => putPosition('BOTTOM', index)}><BiDownArrow /></button>
+        </div>
+        <button className={styles.task__close} onClick={() => deleteTask(order)}>X</button>
+      </div>
+    })}
+
+    <div className={styles.input} >
+      <input type="text" value={newInput} onChange={handleInput} onKeyPress={handleReturn}/>
+    </div>
+
+    <div>
+      <button  disabled={disableButton} onClick={handleLocalstorage}>Salvar</button>
+    </div>
+
     </main>
 
-    <Footer />
+
+    {/* <Footer /> */}
   </>
   )
 }
